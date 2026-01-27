6e5284f613ec0595c9a13fe35d9a1bc1e01a97d1
Mihhail Solovjov <mihhailsolovjov@hotmail.com>
2026-01-27 19:42:26 +0200

    style: format index files

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,13 @@
 export { VersionUtils, CursorUtils } from './utils'
-export { StateManager, type UIState, NavigationManager, type NavigationState, ModalManager, type ModalState, FilterManager, type FilterState } from './state'
+export {
+  StateManager,
+  type UIState,
+  NavigationManager,
+  type NavigationState,
+  ModalManager,
+  type ModalState,
+  FilterManager,
+  type FilterState,
+} from './state'
 export { UIRenderer } from './renderer/index'
 export { InputHandler, ConfirmationInputHandler, type InputAction } from './input-handler'
